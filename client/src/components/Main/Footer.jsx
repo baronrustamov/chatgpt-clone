b920ef0fb878e71c82ec183075d20c83a258edef
spv <spv@spv.sh>
2023-03-14 21:56:03 -0400

    fuck

--- client/src/components/Main/Footer.jsx
+++ client/src/components/Main/Footer.jsx
@@ -4,14 +4,14 @@ export default function Footer() {
   return (
     <div className="px-3 pt-2 pb-3 text-center text-xs text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
       <a
-        href="https://github.com/spv420/chatllama"
+        href="https://github.com/spv420/chatgpt-clone"
         target="_blank"
         rel="noreferrer"
         className="underline"
       >
         ChatLLaMA
       </a>
-      &nbsp;&mdash; a ChatGPT-like digital assistant based on the LLaMA model developed and trained by Meta Platforms.
+      &nbsp;&mdash; a ChatGPT-like digital assistant based on the LLaMA text-generation AI from Meta.
       <br/>
       Credit to <i>danny-avila</i> for the UI,&nbsp;
       <a
